fc77a52ca5e1a470bbe198c27cc414f5325ef4ba
qubit99 <43513960+qubit99@users.noreply.github.com>
2018-12-02 12:13:27 +0530

    Add files via upload

---
+++ meaning.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup
+import urllib.request
+
+"This program finds the meaning of word"
+word=input('enter the word: ')
+website='https://dictionary.cambridge.org/dictionary/english/'+word
+
+req=urllib.request.Request(website)
+with urllib.request.urlopen(req) as response:
+    the_page=response.read()
+
+soup = BeautifulSoup(the_page, 'html.parser')
+source=soup.prettify()
+for i in range(len(source)):
+    if source[i:i+10]=="Learn more":
+        high=i
+        break
+low=264 + 2*len(word)
+source=source[low:high]
+k=str(source).split(":")
+k.pop()
+for j in k:
+    print(j)
